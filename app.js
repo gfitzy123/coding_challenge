82909ee51a16f365618862e556413fd729b590a9
Garrett <gfitzgerald123@gmail.com>
2018-12-11 20:09:47 -0600

    added xml/json handling for locations

--- app.js
+++ app.js
@@ -1,46 +1,70 @@
 const express = require('express')
-const request = require('request');
+const request = require('request-promise')
+const customer_config = require('./customer_config.json')
 const app = express()
 const port = 3000
-
-
+const json2xml = require('json2xml')
+const api_key = 'AIzaSyBSMeUHtkAqvto3YfszErGIRwJ4JJgCV3E'
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-const api_key = 'AIzaSyBSMeUHtkAqvto3YfszErGIRwJ4JJgCV3E'
+app.get('/', function (req, response) {
 
-//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=name, type, language, &locationbias=pont:${lat},${long}&key=YOUR_API_KEY
-
-//nerby search
-//var request = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}, ${long}&name=${name}&key=${api_key}`;
-//app.get('/', (req, res) => res.send('Hello World!'))
-
-//Endpoint accepts request containing parameters
-//lat (latitude)
-//long (longitude)
-//custoner_name (name of customer to search for)
-
-//find place request:
-// https://maps.googleapis.com/maps/api/place/findplacefromtext/output?parameters
-
-
-app.get('/', function (req, res) {
-  console.log(req.query)
-  //console.log(req.params)
   let client_req = req.query
   let lat = client_req.lat
   let long = client_req.long
   let name = client_req.customer_name
+  let api_key = customer_config[name]["api_key"]
+  let type = customer_config[name]["type"].toLowerCase()
+  let language = customer_config[name]["language"].toLowerCase()
+  let response_output = customer_config[name]["response_output"].toLowerCase()
+  let requested_locations = parseInt(customer_config[name]["number_of_nearby_locations_to_request"])
+  let google_api_places_request = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&rankby=distance&language=${language}&key=${api_key}`;
 
-  let google_api_places_request = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?input=${name}&inputtype=textquery&&locationbias=point:${lat},${long}&name=${name}&key=${api_key}`;
-  console.log(google_api_places_request)
-  //res.send(req.params)
-//https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=YOUR_API_KEY
-  request(google_api_places_request , { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    //console.log(res.body)
-     //console.log(body.url);
-    // console.log(body.explanation);
-  });
+  var storage = []
+
+  const fetch = (url, token) => {
+      request(google_api_places_request, { json: true }, (err, res, body) => {
+        if (err) {
+          return console.log(err);
+        }
+      }).then(function(body, res){
+           // check if there are proper amount of objects
+           // in storage, call a recursive function if not
+
+          if (storage.length < requested_locations && body.next_page_token){
+
+            Array.prototype.push.apply(storage, body.results)
+            fetch(google_api_places_request, body.next_page_token)
+
+          } else if (storage.length > requested_locations) {
+            //If storage is greater than requested locatins, we have enough locations now
+            //Just need to trim to the exact amount.
+
+            storage = storage.slice(0, requested_locations + 1)
+
+            if (response_output === 'xml'){
+                console.log('Done - sent ' + storage.length + ' locations back to client.')
+                response.send(json2xml(storage))
+            } else {
+                console.log('Done - sent ' + storage.length + ' locations back to client.')
+                response.send(storage)
+            }
+          } else if (storage.length < requested_locations && !body.next_page_token){
+            //to handle the case where if for some reason, there are no more pages to fulfill the requested_locations
+              if (response_output === 'xml'){
+                  console.log('Done - sent ' + storage.length + ' locations back to client.')
+                  response.send(json2xml(storage))
+              } else {
+                  console.log('Done - sent ' + storage.length + ' locations back to client.')
+                  response.send(storage)
+              }
+          }
+          //Then send results back as JSON or XML,
+      });
+    }
+
+    console.log('Fetching results..')
+    fetch(google_api_places_request)
 
 })
--- spec.js
+++ spec.js
@@ -1,10 +1,13 @@
 const assert = require('assert');
 const request = require('supertest');
+const app = require('./app.js')
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
+describe('GET /user', function() {
+  it('respond with json', function(done) {
+    request(app)
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
   });
 });
